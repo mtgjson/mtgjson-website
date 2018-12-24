57515031afab6c5a61c195126a3e982251c409ec
ZeldaZach <zahalpern+github@gmail.com>
2018-12-23 21:44:51 -0500

    atom

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,8 @@ from pelicanconf import *
 SITEURL = 'https://mtgjson.com'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = 'atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
