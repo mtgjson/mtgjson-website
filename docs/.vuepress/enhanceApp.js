6630780bfe89390742ead43860bbc31ad80e6064
Eric Lakatos <ericlakatos@outlook.com>
2019-04-17 20:47:47 -0400

    merge master and start work.

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export default async ({ Vue }) => {
-  await axios
+  axios
     .get('https://mtgjson.com/json/DeckLists.json')
-    .then(async res => await res.data)
+    .then(res => res.data)
     .then(async data => {
       const decks = await data.decks;
       Vue.prototype.$decks = decks;
@@ -12,25 +12,12 @@ export default async ({ Vue }) => {
       console.error(err);
     });
 
-  await axios
+  axios
     .get('https://mtgjson.com/json/SetList.json')
-    .then(async res => await res.data)
+    .then(res => res.data)
     .then(async data => {
       const sets = await data;
       Vue.prototype.$sets = sets;
-    })
-    .catch(err => {
-      console.error(err);
-    });
-
-  await axios
-    .get(
-      'https://raw.githubusercontent.com/mtgjson/mtgjson-website/master/resources/supporters.json'
-    )
-    .then(async res => await res.data)
-    .then(async data => {
-      const supporters = await data;
-      Vue.prototype.$supporters = supporters;
     })
     .catch(err => {
       console.error(err);
