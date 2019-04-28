04a81e166ad5edcde6473df104fc9dc87dc19421
Eric Lakatos <1939508+staghouse@users.noreply.github.com>
2019-04-27 21:36:52 -0400

    add mtgstocks id and prices schema and documentation. (#159)

--- docs/.vuepress/scripts/Landcycle.js
+++ docs/.vuepress/scripts/Landcycle.js
@@ -1,6 +1,9 @@
 /**
  * @Type Class
  * @constructor @arguments
+ * 
+ * The structure of a mustache is as so:
+ * {{tag:first$second}}
  * */
 
 let logger = null;
@@ -30,7 +33,11 @@ export default class {
 
     let value = card.match(/{{(.*?)}}/)[1];
     let tag = value.split(':')[0];
-    let text = value.split(':')[1];
+    /**
+     * @todo rename these later
+     */
+    let text = card.match(/:(.*?)\$/)[1];
+    let url = card.match(/\$(.*?)}}/)[1] || text;
     let plane = '';
     let land = '';
 
@@ -48,6 +55,10 @@ export default class {
     switch(tag){
       case 'code':
         land = `<code>${text}</code>`;
+        break;
+
+      case 'external':
+        land = `<a class="code-link" href="${url}" target="_blank">${text}</a>`;
         break;
 
       case 'link':
---
+++ docs/structures/prices/README.md
@@ -0,0 +1,32 @@
+---
+{
+  "title": "prices",
+  "schema": "Prices",
+  "meta": [
+    {
+      "name": "description",
+      "content": "prices data structure for MTGJSON.",
+    },
+    {
+      "name": "keywords",
+      "content": "mtg, magic: the gathering, mtgjson, json, prices",
+    }
+  ],
+  "feed": {
+    "enable": "true"
+  }
+}
+---
+
+# Prices
+
+Prices is a structure defining an object of card prices based on date.
+
+> Parent structure: [card](../card), [AllCards](../../files/all-cards)  
+> Parent property: `prices`  
+> Parent property type: `object(anonymous object)`  
+> Property type: `object`  
+
+### Structure
+
+<GenerateTable/>
