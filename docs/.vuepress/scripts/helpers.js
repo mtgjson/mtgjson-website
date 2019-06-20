d3b1ada192137d89f855afbf1e2386e29b150695
Eric Lakatos <1939508+staghouse@users.noreply.github.com>
2019-06-19 21:22:34 -0400

    issue/205 | add: spoiler tag for sets that come out in the future. (#207)
    * issue/205 | add: spoiler tag for sets that come out in the future.
    
    * zach fix

--- docs/.vuepress/scripts/helpers.js
+++ docs/.vuepress/scripts/helpers.js
@@ -10,10 +10,21 @@ export default {
         .join(' ')
     );
   },
+  isFuture: time => {
+    const now = new Date();
+    const thisYear = now.getFullYear();
+    const thisMonth = now.getMonth() + 1; // Buffer the date as January is 0
+    const thisDate = now.getDate();
+    const [year, month, date] = time.split('-').map(c => Number(c));
+
+    // This/next year and next month or
+    // This/next year and this/next month but not today
+    return year >= thisYear && (month > thisMonth || (month == thisMonth && date > thisDate));
+  },
   filter: (filter, dataToFilter) => {
     return filter.length === 0
-      // No filter, return all data
-      ? dataToFilter
+      ? // No filter, return all data
+        dataToFilter
       : dataToFilter.filter(cur => cur.type === filter);
   },
   // A little different than the source but works
@@ -24,7 +35,7 @@ export default {
       : event.split(':');
     const config = {
       prop: values[0],
-      desc: values[1] ? -1 : 1
+      desc: values[1] ? -1 : 1,
     };
 
     const getProp = function(props) {
