e8ecedd268a7a11fbe179ee8a6b68775c3a66e4c
Eric <eric@Erics-MacBook-Pro.local>
2019-12-16 18:55:28 -0500

    issue/346 | add: filtering to downloadable sets for spoilers. add: ability to show and hide the filtering options so they dont get in the way as much.

--- docs/.vuepress/theme/global-components/DownloadDecks.vue
+++ docs/.vuepress/theme/global-components/DownloadDecks.vue
@@ -1,23 +1,27 @@
 <template lang="pug">
   .download-tables
     .sorting-options
-      .sort-row
-        strong Sort By:
-        select.table-sort-select(v-model="sortKey" @change="$helpers.sort(sortKey, decks)")
-          option(value="releaseDate:true" selected) Release Date (Newest)
-          option(value="releaseDate") Release Date (Oldest)
-          option(value="code") Code (Ascending)
-          option(value="code:true") Code (Descending)
-          option(value="name") Name (Ascending)
-          option(value="name:true") Name (Descending)
+      h6.show-options(
+        @click="showOptions = !showOptions"
+        :class="{'hide-options': !showOptions}") Toggle Options
+      .sort-rows(v-if="showOptions")
+        .sort-row
+          strong Sort By:
+          select.table-sort-select(v-model="sortKey" @change="$helpers.sort(sortKey, decks)")
+            option(value="releaseDate:true" selected) Release Date (Newest)
+            option(value="releaseDate") Release Date (Oldest)
+            option(value="code") Code (Ascending)
+            option(value="code:true") Code (Descending)
+            option(value="name") Name (Ascending)
+            option(value="name:true") Name (Descending)
 
-      .sort-row.search
-        strong Search By:
-        input.table-sort-select(
-          placeholder="name, code, etc..."
-          type="text"
-          v-model="searchKey"
-          @input="onHandleChange")
+        .sort-row.search
+          strong Search By:
+          input.table-sort-select(
+            placeholder="name, code, etc..."
+            type="text"
+            v-model="searchKey"
+            @input="onHandleChange")
 
     p.no-result(v-if="decks.length === 0")
       em {{ message }}
@@ -25,20 +29,24 @@
       .download-wrap
         .img-wrap
           div(:class="`ss ss-${deck.code.toLowerCase()}`")
-        .txt-wrap
-          h3(:id="deck.name.replace(/ /g, '_')") {{ deck.name }}
-          ol
-            li
-              small Deck Code:&nbsp;{{ deck.code }}
-            li
-              small Release Date:&nbsp;{{ deck.releaseDate }}
-        ol.dl-wrap
-          li
-            small Downloads:
-            span(v-for="(format, key) in downloadFormats" v-if="format !== 'json'" :key="key")
-              a.dl-btn(:href="`${$env}/${deckDirectory}/${deck.fileName}_${deck.code}.json.${format}`") {{ format }}
-            span(v-else)
-              a.dl-btn(:href="`${$env}/${deckDirectory}/${deck.fileName}_${deck.code}.json`") {{ format }}
+        .text-wrap
+          .text-wrap--details
+            h3(:id="deck.name.replace(/ /g, '_')") {{ deck.name }}
+            ol
+              li
+                small Deck Code:
+                small &nbsp;{{ deck.code }}
+              li
+                small Release Date:
+                small &nbsp;{{ deck.releaseDate }}
+          .text-wrap--downloads
+            ol
+              li
+                small Downloads:
+                span(v-for="(format, key) in downloadFormats" v-if="format !== 'json'" :key="key")
+                  a.dl-btn(:href="`${$env}/${deckDirectory}/${deck.fileName}_${deck.code}.json.${format}`") {{ format }}
+                span(v-else)
+                  a.dl-btn(:href="`${$env}/${deckDirectory}/${deck.fileName}_${deck.code}.json`") {{ format }}
 
 </template>
 
@@ -49,6 +57,7 @@ export default {
     return {
       filteredDecks: [],
       searchKey: '',
+      showOptions: false,
       sortKey: 'releaseDate:true',
       downloadFormats: ['json', 'bz2', 'gz', 'xz', 'zip'],
       deckDirectory: 'json/decks',
--- docs/.vuepress/theme/global-components/DownloadSets.vue
+++ docs/.vuepress/theme/global-components/DownloadSets.vue
@@ -1,33 +1,46 @@
 <template lang="pug">
   .download-tables
     .sorting-options
-      .sort-row
-        strong Filter By:
-        select.table-sort-select(
-          v-model="filterKey"
-          @change="onHandleChange")
-          option(value="" selected) All Sets
-          option(v-for="(type, key) in filters" :value="type") {{ $helpers.prettifyType(type) }}
+      h6.show-options(
+        @click="showOptions = !showOptions"
+        :class="{'hide-options': !showOptions}") Toggle Options
+      .sort-rows(v-if="showOptions")
+        .sort-row
+          label Filter By:
+          select.table-sort-select(
+            v-model="filterKey"
+            @change="onHandleChange")
+            option(value="" selected) All Sets
+            option(v-for="(type, key) in filters" :value="type") {{ $helpers.prettifyType(type) }}
 
-      .sort-row
-        strong Sort By:
-        select.table-sort-select(v-model="sortKey" @change="$helpers.sort(sortKey, sets)")
-          option(value="releaseDate:true") Release Date (Newest)
-          option(value="releaseDate") Release Date (Oldest)
-          option(value="code") Code (Ascending)
-          option(value="code:true") Code (Descending)
-          option(value="name") Name (Ascending)
-          option(value="name:true") Name (Descending)
-          option(value="type") Type (Ascending)
-          option(value="type:true") Type (Descending)
+        .sort-row
+          label Sort By:
+          select.table-sort-select(v-model="sortKey" @change="$helpers.sort(sortKey, sets)")
+            option(value="releaseDate:true") Release Date (Newest)
+            option(value="releaseDate") Release Date (Oldest)
+            option(value="code") Code (Ascending)
+            option(value="code:true") Code (Descending)
+            option(value="name") Name (Ascending)
+            option(value="name:true") Name (Descending)
+            option(value="type") Type (Ascending)
+            option(value="type:true") Type (Descending)
 
-      .sort-row.search
-        strong Search By:
-        input.table-sort-select(
-          placeholder="name, code, etc..."
-          type="text"
-          v-model="searchKey"
-          @input="onHandleChange")
+        .sort-row.search
+          label Search By:
+          input.table-sort-select(
+            placeholder="name, code, etc..."
+            type="text"
+            v-model="searchKey"
+            @input="onHandleChange")
+
+        .sort-row.checkbox
+          label(for="spoilers") Include Spoilers:
+          input(
+            name="spoilers"
+            :checked="spoilerKey"
+            type="checkbox"
+            v-model="spoilerKey"
+            @input="onHandleChange")
 
     p.no-result(v-if="sets.length === 0")
       em {{ message }}
@@ -37,23 +50,27 @@
           div(v-if="set.keyruneCode" :class="`ss ss-${set.keyruneCode.toLowerCase()}`")
           div(v-else-if="set.parentCode" :class="`ss ss-${set.parentCode.toLowerCase()}`")
           div(v-else :class="`ss ss-${set.code.toLowerCase()}`")
-        .txt-wrap
-          h3(:id="set.name.replace(/ /g, '_')" :class="{spoiler: $helpers.isFutureDate(set.releaseDate)}") {{ set.name }}
-          ol
-            li
-              small Set Code:&nbsp;{{ set.code }}
-            li
-              small Type:&nbsp;
-                span {{ set.type.replace('_', ' ') }}
-            li
-              small Release Date:&nbsp;{{ set.releaseDate }}
-        ol.dl-wrap
-          li
-            small Downloads:
-            span(v-for="(format, key) in downloadFormats" v-if="format !== 'json'" :key="key")
-              a.dl-btn(:href="`${$env}/${downloadDirectory}/${set.code}.json.${format}`") {{ format }}
-            span(v-else)
-              a.dl-btn(:href="`${$env}/${downloadDirectory}/${set.code}.json`") {{ format }}
+        .text-wrap
+          .text-wrap--details
+            h3(:id="set.name.replace(/ /g, '_')" :class="{spoiler: $helpers.isFutureDate(set.releaseDate)}") {{ set.name }}
+            ol
+              li
+                small Set Code:
+                small &nbsp;{{ set.code }}
+              li
+                small Type:
+                small &nbsp;{{ set.type.replace('_', ' ') }}
+              li
+                small Release Date:
+                small &nbsp;{{ set.releaseDate }}
+          .text-wrap--downloads
+            ol
+              li
+                small Downloads:
+                span(v-for="(format, key) in downloadFormats" v-if="format !== 'json'" :key="key")
+                  a.dl-btn(:href="`${$env}/${downloadDirectory}/${set.code}.json.${format}`") {{ format }}
+                span(v-else)
+                  a.dl-btn(:href="`${$env}/${downloadDirectory}/${set.code}.json`") {{ format }}
 
 </template>
 
@@ -62,10 +79,12 @@ export default {
   name: 'DownloadSets',
   data() {
     return {
-      filteredSets: [],
+      dynamicSets: [],
       filters: [],
       filterKey: '',
       searchKey: '',
+      spoilerKey: true,
+      showOptions: false,
       sortKey: 'releaseDate:true',
       downloadFormats: ['json', 'bz2', 'gz', 'xz', 'zip'],
       downloadDirectory: 'json',
@@ -75,7 +94,7 @@ export default {
   },
   computed: {
     sets() {
-      return this.filteredSets;
+      return this.dynamicSets;
     },
   },
   async created() {
@@ -83,8 +102,8 @@ export default {
       await this.$store.dispatch('UPDATE_SETS');
     }
 
-    this.filteredSets = this.$helpers.sort('releaseDate:true', this.$store.getters.sets);
-    this.filters = Array.from(new Set(this.filteredSets.map(cur => cur.type)));
+    this.dynamicSets = this.$helpers.sort('releaseDate:true', this.$store.getters.sets);
+    this.filters = Array.from(new Set(this.dynamicSets.map(cur => cur.type)));
   },
   methods: {
     handleMessage(length = 0) {
@@ -95,12 +114,14 @@ export default {
       clearTimeout(this.timeout);
 
       this.timeout = window.setTimeout(() => {
-        const searched = this.$helpers.search(this.searchKey, this.$store.getters.sets);
+        const sets = this.$store.getters.sets;
+        const removed = !this.spoilerKey ? sets.filter(set => !this.$helpers.isFutureDate(set.releaseDate) ) : sets;
+        const searched = this.$helpers.search(this.searchKey, removed);
         const sorted = this.$helpers.sort(this.sortKey, searched);
         const filtered = this.$helpers.filter(this.filterKey, sorted);
 
         this.handleMessage(filtered.length);
-        this.filteredSets = filtered;
+        this.dynamicSets = filtered;
       }, this.$store.getters.throttleSpeed);
     },
   },
