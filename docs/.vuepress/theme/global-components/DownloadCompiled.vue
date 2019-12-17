81b1e2f93a4769ef08a070e4924740aa4adeb6e9
Eric Lakatos <1939508+staghouse@users.noreply.github.com>
2019-12-16 21:45:09 -0500

    issue/fix-compiled | update: css for layout changes. (#351)

--- docs/.vuepress/theme/global-components/DownloadCompiled.vue
+++ docs/.vuepress/theme/global-components/DownloadCompiled.vue
@@ -2,45 +2,46 @@
   .download-tables
     blockquote.download-item(v-if="files" v-for="(file, index) in files")
       .download-wrap
-        .txt-wrap
-          h3 {{ index }}
-          small
-            span(v-html="file.description")
+        .text-wrap
+          .text-wrap--details
+            h3 {{ index }}
+            small
+              span(v-html="file.description")
 
-        ol.dl-wrap
-          //- Individual Files
-          span(v-if="!file.example.includes('Files')")
-            li.dl-wrap--btn-wrap
-              small JSON Files:
-              span
-                a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.json`") JSON
-              span(v-for="(compression, key) in fileBaseCompression")
-                a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.json.${compression}`") {{ compression }}
+          .text-wrap--download
+            //- Individual Files
+            ol(v-if="!file.example.includes('Files')")
+              li.text-wrap--download--btn-wrap
+                small JSON Files:
+                span
+                  a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.json`") JSON
+                span(v-for="(compression, key) in fileBaseCompression")
+                  a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.json.${compression}`") {{ compression }}
 
-          //- Directory Files
-          span(v-if="file.example.includes('Files')")
-            li.dl-wrap--btn-wrap
-              small Directory Files:
-              span(v-for="(format, key) in fileBaseCompression" v-if="format !== 'zip'")
-                a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.tar.${format}`") {{ format }}
-              span(v-else)
-                a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.zip`") {{ format }}
+            //- Directory Files
+            ol(v-if="file.example.includes('Files')")
+              li.text-wrap--download--btn-wrap
+                small Directory Files:
+                span(v-for="(format, key) in fileBaseCompression" v-if="format !== 'zip'")
+                  a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.tar.${format}`") {{ format }}
+                span(v-else)
+                  a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.zip`") {{ format }}
 
-          span(v-if="file.example === 'AllPrintings'")
-            li.dl-wrap--btn-wrap
-              small SQL Files:
-              span
-                a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.sql`") SQL
-              span(v-for="(compression, key) in fileBaseCompression")
-                a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.sql.${compression}`") {{ compression }}
-            li.dl-wrap--btn-wrap
-              small SQLite Files:
-              span
-                a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.sqlite`") SQLite
-              span(v-for="(compression, key) in fileBaseCompression")
-                a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.sqlite.${compression}`") {{ compression }}
-            li.sqlite
-              small SQL/SQLite database courtesy of <a href="https://github.com/mtgjson/mtgsqlive" target="_blank">mtgsqlive</a>.
+            ol(v-if="file.example === 'AllPrintings'")
+                li.text-wrap--download--btn-wrap
+                  small SQL Files:
+                  span
+                    a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.sql`") SQL
+                  span(v-for="(compression, key) in fileBaseCompression")
+                    a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.sql.${compression}`") {{ compression }}
+                li.text-wrap--download--btn-wrap
+                  small SQLite Files:
+                  span
+                    a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.sqlite`") SQLite
+                  span(v-for="(compression, key) in fileBaseCompression")
+                    a.dl-btn(:href="`${$env}/${fileDirectory}/${file.example}.sqlite.${compression}`") {{ compression }}
+                li.sqlite
+                  p SQL/SQLite database courtesy of <a href="https://github.com/mtgjson/mtgsqlive" target="_blank">mtgsqlive</a>.
 
 </template>
 
@@ -51,17 +52,17 @@ export default {
     return {
       files: require(`../../public/schemas/Files.schema.json`),
       fileBaseCompression: ['bz2', 'gz', 'xz', 'zip'],
-      fileSQLCompression: ['sql','sqlite'],
+      fileSQLCompression: ['sql', 'sqlite'],
       fileDirectory: 'files',
     };
   },
-  created(){
+  created() {
     let files = this.files;
     const landcycle = new this.$landcycle(files);
     landcycle._init();
 
     // Remove quotes
-    for(let file in files){
+    for (let file in files) {
       if (hasOwnProperty.call(files, file)) {
         const { example } = files[file];
 
@@ -69,31 +70,44 @@ export default {
       }
     }
 
-
     this.files = landcycle.schema;
-  }
+  },
 };
 </script>
 
 <style lang="stylus" scoped>
 @require '../styles/download';
 
-.txt-wrap {
-  small {
-    span {
-      text-transform: none !important;
-    }
-  }
-}
+.download-tables {
+  .download {
+    &-item {
+      &:first-of-type {
+        border-color: orange;
+      }
 
-.download-item:first-of-type {
-  border-color:orange;
-}
+      .download-wrap {
+        .text-wrap {
+          small {
+            span {
+              display: block;
+              font-weight: normal !important;
+              text-transform: none !important;
+              margin-bottom: 15px;
+            }
+          }
 
-.dl-wrap {
-  &--btn-wrap {
-    small {
-      color: var(--gray-color)
+          ol {
+            li {
+              display: block;
+              margin-bottom: 10px;
+
+              small {
+                margin-right: 5px;
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
