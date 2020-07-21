2d47f2136595c794e23c3af69ff34c50cea32fae
Eric <eric@Erics-MacBook-Pro.local>
2020-07-21 19:29:42 -0400

    fix: mobile a11y scaling.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,7 +30,7 @@ module.exports = {
       'meta',
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, maximum-scale=1,user-scalable=no'
+        content: 'width=device-width, initial-scale=1'
       }
     ],
     // Favicon and OS Tiles
