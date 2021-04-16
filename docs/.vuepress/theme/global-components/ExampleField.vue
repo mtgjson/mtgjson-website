0e55ceec1cd688e6a56659b74678ce64f0ffacfb
Eric Lakatos <lakatos.eric@gmail.com>
2021-04-16 15:36:35 -0400

    docs: minor updates to ExampleField and Identifiers

--- docs/.vuepress/theme/global-components/ExampleField.vue
+++ docs/.vuepress/theme/global-components/ExampleField.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   .example-field(v-if="enums.length > 0" :class="{'showing': showAll}")
     strong Examples:{{' '}}
-    code(v-if="!showAll") {{ enums.slice(0, 5).toString().split(',').join(', ') }}
+    code(v-if="!showAll") {{ shortenEnums(enums) }}
       div.show-btn(v-if="enums.length > 5") ,{{' '}}
         span(@click="toggleShowAll") Show&nbsp;More
-    code(v-if="showAll && enums.length > 5") {{ enums.toString().split(',').join(', ') }},{{' '}}
+    code(v-if="showAll && enums.length > 5") {{ elongateEnums(enums) }},{{' '}}
       div.show-btn
         span(@click="toggleShowAll") Show&nbsp;Less
 </template>
@@ -45,6 +45,12 @@ export default {
   methods: {
     toggleShowAll(){
       this.show = !this.show;
+    },
+    shortenEnums(enums){
+      return enums.sort().slice(0, 5).toString().split(',').join(', ')
+    },
+    elongateEnums(enums){
+      return enums.sort().toString().split(',').join(', ');
     }
   }
 };
@@ -57,9 +63,7 @@ export default {
   align-items: center;
 
   code {
-    padding-top: 0;
-    padding-bottom: 0;
-    line-height: 1.7rem;
+    line-height: 1.25rem;
   }
 
   .show-btn {
