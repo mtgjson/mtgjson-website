a4a5bf37d265c4da607a690f1271b0e985d6ccdc
Eric Lakatos <lakatos.eric@gmail.com>
2021-12-29 17:00:02 -0500

    update: check version script to run pre-install (although this is broken in some versions of npm, it will at least run post-install).

--- check-version.js
+++ check-version.js
@@ -1,9 +1,14 @@
 // https://medium.com/@adambisek/how-to-check-minimum-required-node-js-version-4a78a8855a0f
+import chalk from 'chalk';
 import semver from 'semver';
 import { engines } from './package';
 
 const version = engines.node;
-if (!semver.satisfies(process.version, version)) {
-  console.log(`Required node version ${version} not satisfied with current version ${process.version}.`);
+const satisfactory = semver.satisfies(process.version, version);
+
+if (satisfactory) {
+  console.log(chalk.green(`Required Node version ${version} is satisfied with current version ${process.version}.`));
+} else {
+  console.log(chalk.red(`Required node version ${version} not satisfied with current version ${process.version}.`));
   process.exit(1);
 }
