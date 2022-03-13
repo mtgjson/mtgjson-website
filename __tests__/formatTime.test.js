5529ca9275fa85a348bf27079a2b32292a657416
Eric Lakatos <eric.lakatos@disney.com>
2022-03-12 21:52:30 -0500

    issue/530: Change formatTime and tests to include years.

--- __tests__/formatTime.test.js
+++ __tests__/formatTime.test.js
@@ -4,18 +4,72 @@ describe('formatTime', () => {
   it('should return a single month', () => {
     const inputTime = '2020-12';
     const overrideTime = '2020-12';
-    const expectedTimeString = 'Month)';
     const returnedTimeString = formatTime(inputTime, overrideTime);
 
-    expect(returnedTimeString.includes(expectedTimeString)).toEqual(true);
+    expect(returnedTimeString).toBe('(1 Month)');
   });
 
   it('should return a multiple months', () => {
-    const inputTime = '2020-12';
-    const expectedTimeString = 'Months)';
+    const inputTime = '2020-11';
+    const overrideTime = '2020-12';
+    const returnedTimeString = formatTime(inputTime, overrideTime);
+
+    expect(returnedTimeString).toBe('(2 Months)');
+  });
+
+  it('should return a single year', () => {
+    const inputTime = '2021-01';
+    const overrideTime = '2021-12';
+    const returnedTimeString = formatTime(inputTime, overrideTime);
+
+    expect(returnedTimeString).toBe('(1 Year)');
+  });
+
+  it('should return a multiple years', () => {
+    const inputTime = '2021-01';
+    const overrideTime = '2022-12';
+    const returnedTimeString = formatTime(inputTime, overrideTime);
+
+    expect(returnedTimeString).toBe('(2 Years)');
+  });
+
+  it('should return a single year and single month', () => {
+    const inputTime = '2021-01';
+    const overrideTime = '2022-01';
+    const returnedTimeString = formatTime(inputTime, overrideTime);
+
+    expect(returnedTimeString).toBe('(1 Year, 1 Month)');
+  });
+
+  it('should return a multiple years and single month', () => {
+    const inputTime = '2021-01';
+    const overrideTime = '2023-01';
+    const returnedTimeString = formatTime(inputTime, overrideTime);
+
+    expect(returnedTimeString).toBe('(2 Years, 1 Month)');
+  });
+
+  it('should return a single year and multiple months', () => {
+    const inputTime = '2021-01';
+    const overrideTime = '2022-02';
+    const returnedTimeString = formatTime(inputTime, overrideTime);
+
+    expect(returnedTimeString).toBe('(1 Year, 2 Months)');
+  });
+
+  it('should return a multiple years and multiple months', () => {
+    const inputTime = '2021-01';
+    const overrideTime = '2023-02';
+    const returnedTimeString = formatTime(inputTime, overrideTime);
+
+    expect(returnedTimeString).toBe('(2 Years, 2 Months)');
+  });
+
+  it('should return something', () => {
+    const inputTime = '2021-01';
     const returnedTimeString = formatTime(inputTime);
 
-    expect(returnedTimeString.includes(expectedTimeString)).toEqual(true);
+    expect(returnedTimeString.includes(')')).toBe(true);
   });
 
   it('should return an empty string if bad data is passed', () => {
