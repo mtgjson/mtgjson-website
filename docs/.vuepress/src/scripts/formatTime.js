682cebbf124c03f85cde568df00a8f9350add271
Eric Lakatos <lakatos.eric@gmail.com>
2022-03-12 21:58:50 -0500

    issue/530: Change formatTime and tests to include years. (#531)
    Co-authored-by: Eric Lakatos <eric.lakatos@disney.com>

--- docs/.vuepress/src/scripts/formatTime.js
+++ docs/.vuepress/src/scripts/formatTime.js
@@ -11,19 +11,43 @@ export default (time, overrideTime) => {
   let year = newDate.getFullYear();
   let month = newDate.getMonth() + 1; // +1 because arrays are 0
 
+  let totalYears = 0;
   let totalMonths = 0;
+  let remainderMonths = 0;
 
-  if(overrideTime && typeof overrideTime === 'string'){
+  if (overrideTime && typeof overrideTime === 'string') {
     newDate = overrideTime.split('-');
     year = Number(newDate[0]);
     month = Number(newDate[1]);
   }
 
   totalMonths = 12 * (year - sinceYear) + (month - sinceMonth) + 1;
+  totalYears = Math.floor(totalMonths / 12);
+  remainderMonths = totalMonths % 12;
 
-  if (totalMonths === 1) {
-    return `(1 Month)`;
+  if (totalYears < 1) {
+    if (totalMonths > 1) {
+      return `(${totalMonths} Months)`;
+    } else {
+      return `(${totalMonths} Month)`;
+    }
+  } else {
+    if (totalYears < 2) {
+      if (remainderMonths > 1) {
+        return `(${totalYears} Year, ${remainderMonths} Months)`;
+      } else if (remainderMonths === 1) {
+        return `(${totalYears} Year, ${remainderMonths} Month)`;
+      } else {
+        return `(${totalYears} Year)`;
+      }
+    } else {
+      if (remainderMonths > 1) {
+        return `(${totalYears} Years, ${remainderMonths} Months)`;
+      } else if (remainderMonths === 1) {
+        return `(${totalYears} Years, ${remainderMonths} Month)`;
+      } else {
+        return `(${totalYears} Years)`;
+      }
+    }
   }
-
-  return `(${totalMonths} Months)`;
 };
